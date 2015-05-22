1144abbe52841ef3dc36d6f355853abeb0f20064
Jaylord Ferrer <simplyniceweb@gmail.com>
2015-05-22 17:05:47 +0800

    pre test

--- web/index.php
+++ web/index.php
@@ -1,25 +1,22 @@
 <?php
 
-	define('ENVIRONMENT', 'production'); // development // production
+define('ENVIRONMENT', 'development'); // development // production
 
-	if (defined('ENVIRONMENT'))
-	{
-		switch (ENVIRONMENT)
-		{
-			case 'production':
-				error_reporting(0);
-				ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT & ~E_DEPRECATED);
-				ini_set('display_errors','Off');
-			break;
-			
-			case 'development':
-			default:
-				error_reporting(E_ALL);
-			break;
-		}
+if (defined('ENVIRONMENT')) {
+	switch (ENVIRONMENT) {
+		case 'production':
+			error_reporting(0);
+			ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT & ~E_DEPRECATED);
+			ini_set('display_errors','Off');
+		break;
+		
+		case 'development':
+		default:
+			error_reporting(E_ALL);
+		break;
 	}
-	
-	require_once(__DIR__ . '/../application/bootstrap.php');
+}
 
-	$app->run();
-?>
+require_once(__DIR__ . '/../application/bootstrap.php');
+
+$app->run();
